bab556fa76488ea10b98f6b70cfdbaa5ece6fd60
Tomas Janousek <tomi@nomi.cz>
2014-05-12 14:24:54 +0200

    Support opts using -proper_opts( [ ... ] ) syntax

--- src/pt_proper_eunit.erl
+++ src/pt_proper_eunit.erl
@@ -3,28 +3,64 @@
 
 parse_transform( AST, _Options ) ->
 	Forms = erl_syntax:form_list( AST ),
+	% TODO: use analyze_forms
+	Opts = erl_syntax_lib:fold( fun find_opts/2, [], Forms ),
 	Props = erl_syntax_lib:fold( fun find_props/2, [], Forms ),
-	PropForms = lists:map( fun make_test/1, Props ),
+	PropForms = [ test_generator( Prop, Opts ) || Prop <- Props ],
 	erl_syntax:revert_forms( [ Forms | PropForms ] ).
+
+find_opts( Node, Opts ) ->
+	try
+		{ proper_opts, Value } = erl_syntax_lib:analyze_wild_attribute( Node ),
+		lists:map( fun opt_to_property/1, Value ) ++ Opts
+	catch
+		throw:syntax_error    -> Opts;
+		error:{ badmatch, _ } -> Opts
+	end.
+
+opt_to_property( X ) ->
+	case is_prop_opt( X ) of
+		true  -> X;
+		false -> { global, X }
+	end.
+
+is_prop_opt( { Name, _ } ) ->
+	is_prop_name( Name );
+is_prop_opt( _ ) ->
+	false.
+
+is_prop_name( Name ) when is_atom( Name ) ->
+	is_prop_name( atom_to_list( Name ) );
+is_prop_name( Name ) when is_list( Name ) ->
+	lists:prefix( "prop_", Name ).
 
 find_props( Node, Props ) ->
 	try
 		{ Name, 0 } = erl_syntax_lib:analyze_function( Node ),
-		NameStr = atom_to_list( Name ),
-		true = lists:prefix( "prop_", NameStr ),
-		[ { NameStr, pos( Node ) } | Props ]
+		true = is_prop_name( Name ),
+		[ { Name, pos( Node ) } | Props ]
 	catch
 		throw:syntax_error    -> Props;
 		error:{ badmatch, _ } -> Props
 	end.
 
-make_test( { Name, Pos } ) ->
-	TestName = Name ++ "_test_",
-	IsTrue = assert( call_quickcheck( call_prop( Name ), erl_syntax:nil() ) ),
-	Fun = erl_syntax:fun_expr( [ erl_syntax:clause( none, [ IsTrue ] ) ] ),
-	Tuple = erl_syntax:tuple( [ Pos, Fun ] ),
+pos( X ) ->
+	erl_syntax:abstract( erl_syntax:get_pos( X ) ).
+
+test_generator( { Name, Pos }, AllOpts ) ->
+	TestName = atom_to_list( Name ) ++ "_test_",
+	Opts = proplists:get_all_values( Name, AllOpts )
+		++ proplists:get_all_values( global, AllOpts ),
+	Tuple = erl_syntax:tuple( [ Pos, test_fun_expr( Name, Opts ) ] ),
 	erl_syntax:function( erl_syntax:atom( TestName ),
 		[ erl_syntax:clause( none, [ Tuple ] ) ] ).
+
+test_fun_expr( Name, Opts ) ->
+	Assert = assert( call_quickcheck( call_prop( Name ), erl_syntax:abstract( Opts ) ) ),
+	erl_syntax:fun_expr( [ erl_syntax:clause( none, [ Assert ] ) ] ).
+
+assert( X ) ->
+	erl_syntax:match_expr( erl_syntax:atom( true ), X ).
 
 call_prop( Name ) ->
 	erl_syntax:application( erl_syntax:atom( Name ), [] ).
@@ -32,12 +68,6 @@ call_prop( Name ) ->
 call_quickcheck( Test, Opts ) ->
 	erl_syntax:application( erl_syntax:atom( proper ),
 		erl_syntax:atom( quickcheck ), [ Test, Opts ] ).
-
-assert( X ) ->
-	erl_syntax:match_expr( erl_syntax:atom( true ), X ).
-
-pos( X ) ->
-	erl_syntax:abstract( erl_syntax:get_pos( X ) ).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- test/pt_proper_eunit_tests.erl
+++ test/pt_proper_eunit_tests.erl
@@ -3,11 +3,13 @@
 
 -define( EUNIT_NOAUTO, 1 ).
 -include( "pt_proper_eunit.hrl" ).
+-proper_opts( [ { prop_3, any_to_integer } ] ).
 
 tests() ->
 	Opts = [ no_tty ],
 	[ ?_assertMatch( error, eunit:test( { generator, fun prop_1_test_/0 }, Opts ) )
 	, ?_assertMatch( ok   , eunit:test( { generator, fun prop_2_test_/0 }, Opts ) )
+	, ?_assertMatch( ok   , eunit:test( { generator, fun prop_3_test_/0 }, Opts ) )
 	].
 
 prop_1() ->
@@ -15,3 +17,6 @@ prop_1() ->
 
 prop_2() ->
 	?FORALL( X, integer( 0, 10 ), X =:= X ).
+
+prop_3() ->
+	?FORALL( X, any(), is_integer( X ) ).
